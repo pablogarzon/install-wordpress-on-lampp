116fa72552b5adf2de5ae5a84eb6f142aebb1f16
notebook <pablofgarzon@gmail.com>
2017-11-08 02:41:38 -0300

    refining the script

--- install-wordpress.sh
+++ install-wordpress.sh
@@ -1,16 +1,25 @@
 #!/bin/sh
 
-echo 'The name of the WordPress site'
+echo 'The name of the WordPress site:'
 read SITE
-echo 'The name of the database for WordPress'
+echo 'The name of the database for WordPress:'
 read DB_NAME
-echo 'MySQL database username'
+
+
+### by default database user should be 'root' and database password is ''
+DB_USER='YOUR_DATABASE_USER';
+DB_PASSWORD='YOUR_DATABASE_PASSWORD';
+
+### if you always use the same db account comment out/delete the below lines:
+echo 'mysql database username:'
 read DB_USER
-echo 'MySQL database password'
+echo 'mysql database password:'
 read DB_PASSWORD
 
-DIR=${PWD}
 
+/opt/lampp/lampp start;
+
+echo Downloading and setting up the site...
 curl -sS https://wordpress.org/latest.zip > wordpress.zip 		&& \
 unzip wordpress.zip                                  			&& \
 rm wordpress.zip							&& \
@@ -19,16 +28,20 @@ mv ./wordpress/wp-config-sample.php ./wordpress/wp-config.php		&& \
 sed -i "s/database_name_here/${DB_NAME}/" ./wordpress/wp-config.php 	&& \
 sed -i "s/username_here/${DB_USER}/" ./wordpress/wp-config.php 		&& \
 sed -i "s/password_here/${DB_PASSWORD}/" ./wordpress/wp-config.php	&& \
-cd /									&& \
-mv $DIR/wordpress opt/lampp/htdocs/$SITE
+mv ./wordpress /opt/lampp/htdocs/$SITE
 
-### create the database on phpmyadmin
-/opt/lampp/bin/mysql -u$DB_USER -p$DB_PASSWORD -e"create database if not exists ${DB_NAME}"
+echo Create the database on phpmyadmin...
+/opt/lampp/bin/mysql -u$DB_USER -p$DB_PASSWORD -e"create database if not exists ${DB_NAME} CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;"
 
-### ftp settings
-### by default FTP_USER should be daemon
-### by default FTP_HOST should be localhost
-### by default FTP_PASS should be empty
+
+################################## ftp settings #####################################
+### by default FTP_USER should be 'daemon'
+### by default FTP_HOST should be 'localhost'
+### by default FTP_PASS should be ''
+
 #echo "define('FTP_USER', 'YOUR-FTP-USER');" >> opt/lampp/htdocs/$SITE/wp-config.php
-#echo "define('FTP_HOST', 'localhost');" >> opt/lampp/htdocs/$SITE/wp-config.php 	
+#echo "define('FTP_HOST', 'localhost');" >> opt/lampp/htdocs/$SITE/wp-config.php
 #echo "define('FTP_PASS', 'YOUR-PASS');" >> opt/lampp/htdocs/$SITE/wp-config.php
+#####################################################################################
+
+xdg-open http://localhost/$SITE
